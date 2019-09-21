1444bc80737b21f4033bbf618612522a07a9fa4a
Hoàng Đăng Khoa <hoangdangkhoa.m9@gmail.com>
2019-09-21 19:30:00 +0700

    update UI

--- Project/LunchOffice_App/LunchOffice_App/LunchOffice_App.Android/obj/Debug/90/android/src/mono/android/app/environment.x86.s
+++ Project/LunchOffice_App/LunchOffice_App/LunchOffice_App.Android/obj/Debug/90/android/src/mono/android/app/environment.x86.s
@@ -64,7 +64,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"8f0488c4-2703-449e-881f-d1a64d85dc42"
+	.asciz	"e839bba0-fb76-4aa9-870b-3105b6b61dca"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
